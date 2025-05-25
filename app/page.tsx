6267a477599b761780d557292d2f3a9ec18e1176
nathanRael <ralaivoavy.natanael@gmail.com>
2025-05-25 10:38:20 +0300

    chore : temporary change profile image

--- app/page.tsx
+++ app/page.tsx
@@ -4,35 +4,44 @@ import ProjectsSection from "@/sections/Projects";
 import SkillsSection from "@/sections/Skills";
 import ContactSection from "@/sections/Contact";
 import Footer from "@/sections/Footer";
-import {Suspense} from "react";
-import {PROFILE_IMAGE_QUERY, PROJECT_QUERY, RESUME_QUERY, SKILL_QUERY} from "@/sanity/lib/query";
-import {sanityFetch} from "@/sanity/lib/live";
+import { Suspense } from "react";
+import { PROJECT_QUERY, RESUME_QUERY, SKILL_QUERY } from "@/sanity/lib/query";
+import { sanityFetch } from "@/sanity/lib/live";
 
-export const revalidate = 3600
+export const revalidate = 3600;
 
-export default async function Home({searchParams}: { searchParams: Promise<{ filter?: string }> }) {
-    const filter = (await searchParams).filter;
-    const { data: resumeData } = await sanityFetch({ query : RESUME_QUERY});
-    const { data: profileImage } = await sanityFetch({ query : PROFILE_IMAGE_QUERY});
-    const { url: resumeUrl } = resumeData;
-    const { url: profileImageUrl } = profileImage;
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ filter?: string }>;
+}) {
+  const filter = (await searchParams).filter;
+  const { data: resumeData } = await sanityFetch({ query: RESUME_QUERY });
+  // const { data: profileImage } = await sanityFetch({
+  //   query: PROFILE_IMAGE_QUERY,
+  // });
+  const { url: resumeUrl } = resumeData;
+  // const { url: profileImageUrl } = profileImage;
 
-    const { data: projects } = await sanityFetch({query : PROJECT_QUERY});
-    const { data: skills } = await sanityFetch({ query : SKILL_QUERY});
+  const { data: projects } = await sanityFetch({ query: PROJECT_QUERY });
+  const { data: skills } = await sanityFetch({ query: SKILL_QUERY });
 
+  return (
+    <section className="p-6 flex gap-[256px] max-md:gap-[128px]  flex-col items-center justify-center w-full">
+      <Navbar />
 
-    return (
-        <section className="p-6 flex gap-[256px] max-md:gap-[128px]  flex-col items-center justify-center w-full">
-            <Navbar/>
-            <HeroSection profileImageUrl={profileImageUrl} resumeUrl={resumeUrl}/>
-            <Suspense>
-                <ProjectsSection projects={projects} filter={filter}/>
-            </Suspense>
-            <SkillsSection skills={skills}/>
-            <div id={"contact"} className={'w-full'}>
-                <ContactSection/>
-            </div>
-            <Footer/> 
-        </section>
-    );
+      <HeroSection
+        profileImageUrl={"/images/profile-transparent.png"}
+        resumeUrl={resumeUrl}
+      />
+      <Suspense>
+        <ProjectsSection projects={projects} filter={filter} />
+      </Suspense>
+      <SkillsSection skills={skills} />
+      <div id={"contact"} className={"w-full"}>
+        <ContactSection />
+      </div>
+      <Footer />
+    </section>
+  );
 }
--- sections/Skills.tsx
+++ sections/Skills.tsx
@@ -2,25 +2,35 @@
 import AnimatedText from "@/components/ui/AnimatedText";
 
 
-export  default async function SkillsSection({skills} : {skills: Skill[]}) {
+export default async function SkillsSection({ skills }: { skills: Skill[] }) {
+    const experimentedSkills = skills.filter(skill => skill.experimented);
+    const usedSkills = skills.filter(skill => !skill.experimented);
 
-    
-    const experimentedSkills = (skills as Skill[]).filter(skill => skill.experimented)
-    const usedSkills = (skills as Skill[]).filter(skill => !skill.experimented)
-    
-    
     return (
-        <section id={"skills"} className="section !gap-20">
-            <div className={"flex flex-col items-center justify-start gap-10"}>
-                <AnimatedText whileInView={"visible"} initial={"hidden"} custom={1} className={'text-white-100 text-subtitle font-semibold -rotate-6 pb-10'}>I&apos;ve been <span
-                    className={'text-secondary-100'}>using</span></AnimatedText>
-                    <SkillList skills={usedSkills}/>
-            </div>            
-            <div className={"flex flex-col items-center justify-start gap-10"}>
-                <AnimatedText  whileInView={"visible"} initial={"hidden"} custom={2} className={'text-white-100 text-subtitle font-semibold '}>and <span
-                    className={'text-yellow-300'}>exprimenting</span></AnimatedText>
-                <SkillList skills={experimentedSkills}/>
+        <section id="skills" className="section !gap-20">
+            <div className="flex flex-col items-center justify-start gap-10">
+                <AnimatedText
+                    whileInView="visible"
+                    initial="hidden"
+                    custom={1}
+                    className="text-white-100 text-subtitle font-semibold -rotate-6 pb-10"
+                >
+                    I&apos;ve been <span className="text-secondary-100">working with</span>
+                </AnimatedText>
+                <SkillList skills={usedSkills} />
+            </div>
+
+            <div className="flex flex-col items-center justify-start gap-10">
+                <AnimatedText
+                    whileInView="visible"
+                    initial="hidden"
+                    custom={2}
+                    className="text-white-100 text-subtitle font-semibold"
+                >
+                    and <span className="text-yellow-300">experimenting with</span>
+                </AnimatedText>
+                <SkillList skills={experimentedSkills} />
             </div>
         </section>
-    )
+    );
 }
